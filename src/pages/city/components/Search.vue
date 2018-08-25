3d2ab0701ed7901ecd9b2a8ac0638cdaf1980cd7
yangdilin <1004137895@qq.com>
2018-08-25 22:13:17 +0800

    two

--- src/pages/city/components/Search.vue
+++ src/pages/city/components/Search.vue
@@ -1,12 +1,53 @@
 <template>
-  <div class="search">
-    <input type="text" placeholder="输入城市名或拼音" class="input">
+  <div>
+    <div class="search">
+      <input type="text" placeholder="输入城市名或拼音" class="input" v-model="keyword">
+    </div>
+    <div class="search-content" ref="ul" v-show="keyword">
+      <ul >
+        <li class="content border-bottom" v-for="item in arr" :key="item.id">{{item.name}}</li>
+        <li class="content border-bottom" v-show="hasNoData">没有查询到任何记录</li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
+import BScroll from 'better-scroll'
 export default {
-  name: 'CitySearch'
+  name: 'CitySearch',
+  props: {
+    cities: Object
+  },
+  data () {
+    return {
+      keyword: '',
+      arr: []
+    }
+  },
+  computed: {
+    hasNoData () {
+      return this.arr.length === 0
+    }
+  },
+  watch: {
+    keyword () {
+      this.arr = []
+      if (!this.keyword) {
+        return
+      }
+      for (let i in this.cities) {
+        this.cities[i].forEach((item) => {
+          if (item.name.indexOf(this.keyword) > -1 || item.spell.indexOf(this.keyword) > -1) {
+            this.arr.push(item)
+          }
+        })
+      }
+    }
+  },
+  mounted () {
+    this.scroll = new BScroll(this.$refs.ul)
+  }
 }
 </script>
 
@@ -16,13 +57,27 @@ export default {
     background $bgColor
     height: .7rem
     padding:0 .1rem
-  .input
-    height: .6rem
-    display block
-    width: 100%
-    border-radius .1rem
-    padding-left: .1rem
-    box-sizing border-box
-    text-align: center
-    color #666
+    .input
+      height: .6rem
+      display block
+      width: 100%
+      border-radius .1rem
+      padding-left: .1rem
+      box-sizing border-box
+      text-align: center
+      color #666
+  .search-content
+    z-index 1
+    position: absolute
+    top:1.58rem
+    left: 0
+    right: 0
+    bottom 0
+    background: #ccc
+    overflow hidden
+    .content
+      line-height: .78rem
+      height: .78rem
+      background: #fff
+      padding-left: .2rem
 </style>
